54ffe9e5552daa9f1d4d0bb6f0c6b1fb27e5d74a
Thinh <ttthinh2904@gmail.com>
2026-04-21 08:17:05 +0700

    first commit

---
+++ src/test/java/org/example/stationery_shop/StationeryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stationery_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationeryShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
